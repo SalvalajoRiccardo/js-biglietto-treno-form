7dbeb01715b6d4ffd9e33410a0e9ed6c008a0eaa
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-04 16:22:58 +0200

    prendi dati

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("genera").addEventListener("click", function(){
+    
+    var nome = document.getElementById("identita").value;
+    console.log(nome);
+    var km = document.getElementById("km").value;
+    console.log(km);
+    var eta = document.getElementById("eta").value;
+    console.log(eta);
+  }); 
